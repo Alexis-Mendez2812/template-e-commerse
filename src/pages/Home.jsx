290005ad4b701e8b85afdba13fecaeb7b3fecea2
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-08-06 23:36:00 -0300

    footer-home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,50 @@
+import React from "react";
+import Helmet from "../components/Helmet/Helmet.js";
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero.png";
+import { Link } from "react-router-dom";
 
-
-import React from 'react'
-
+import "../styles/hero-section.css";
 const Home = () => {
-  return (
-    <div>
-      Home
-    </div>
-  )
-}
+	return (
+		<Helmet title="Home">
+			<section>
+				<Container>
+					<Row>
+						<Col lg="6" md="6">
+							<div className="hero__content">
+								<h5 className="mb-3">Easy way to make an order</h5>
+								<h1 className="mb-4 hero__title ">
+									<span>HUNGRY?</span> Just wait <br></br> food at
+									<span> your door</span>
+								</h1>
+								<p>
+									Lorem ipsum dolor sit amet consectetur adipisicing elit.
+									Facere, impedit aliquid dicta voluptas harum sequi!
+								</p>
+								<div className="hero__btns d-flex align-items-center gap-5 mt-4 ">
+									<button className="order__btn  d-flex align-items-center justify-content-between">
+										Order now
+										<i className="ri-arrow-right-s-line"></i>
+									</button>
+									<button>
+										<Link className="all__foods-btn" to="/foods">
+											See all foods
+										</Link>
+									</button>
+								</div>
+							</div>
+						</Col>
+						<Col lg="6" md="6">
+							<div className="hero__img">
+								<img src={heroImg} alt="hero-Img" className="w-100" />
+							</div>
+						</Col>
+					</Row>
+				</Container>
+			</section>
+		</Helmet>
+	);
+};
 
-export default Home
+export default Home;
