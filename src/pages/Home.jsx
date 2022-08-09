c8f692cfc3b81502d46fe4132d4aac1001e6cd8b
Alexis-Mendez2812 <281212.namaste@gmail.com>
2022-08-08 23:18:17 -0300

    add cards and state

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,69 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Helmet from "../components/Helmet/Helmet.js";
 import { Container, Row, Col } from "reactstrap";
 import heroImg from "../assets/images/hero.png";
 import { Link } from "react-router-dom";
 
 import "../styles/hero-section.css";
+import Category from "../components/UI/category/Category.jsx";
+import "../styles/home.css";
+import featureImg01 from "../assets/images/service-01.png";
+import featureImg02 from "../assets/images/service-02.png";
+import featureImg03 from "../assets/images/service-03.png";
+
+import products from "../assets/fake-data/products.js";
+
+import foodCategoryImg01 from "../assets/images/hamburger.png";
+import foodCategoryImg02 from "../assets/images/pizza.png";
+import foodCategoryImg03 from "../assets/images/bread.png";
+
+import ProductCard from "../components/UI/product-card/ProductCard.jsx";
+
+const featureData = [
+	{
+		title: "Quick Delivery",
+		imgUrl: featureImg01,
+		desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+	{
+		title: "Super Dine In",
+		imgUrl: featureImg02,
+		desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+	{
+		title: "Easy Pick Up",
+		imgUrl: featureImg03,
+		desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+	},
+];
 const Home = () => {
+	const [category, setCategory] = useState("ALL");
+	const [allProducts, setAllProducts] = useState(products);
+
+	useEffect(() => {
+		if (category === "ALL") {
+			setAllProducts(products);
+		}
+		if (category === "BURGER") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "Burger"
+			);
+			setAllProducts(filteredProducts);
+		}
+		if (category === "PIZZA") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "Pizza"
+			);
+			setAllProducts(filteredProducts);
+		}
+		if (category === "BREAD") {
+			const filteredProducts = products.filter(
+				(item) => item.category === "Bread"
+			);
+			setAllProducts(filteredProducts);
+		}
+	}, [category]);
+
 	return (
 		<Helmet title="Home">
 			<section>
@@ -27,11 +85,25 @@ const Home = () => {
 										Order now
 										<i className="ri-arrow-right-s-line"></i>
 									</button>
-									<button>
-										<Link className="all__foods-btn" to="/foods">
-											See all foods
-										</Link>
+									<button className="all__foods-btn">
+										<Link to="/foods">See all foods</Link>
 									</button>
+								</div>
+								<div className=" hero__service d-flex align-items-center gap-5 mt-5">
+									<p className=" d-flex align-items-center gap-2">
+										<span className="shipping__icon">
+											{" "}
+											<i class="ri-car-line"></i>
+										</span>{" "}
+										No shipping charge{" "}
+									</p>
+									<p className=" d-flex align-items-center gap-2">
+										<span className="shipping__icon">
+											{" "}
+											<i class="ri-shield-check-line"></i>
+										</span>{" "}
+										100% secure checkout
+									</p>
 								</div>
 							</div>
 						</Col>
@@ -40,6 +112,76 @@ const Home = () => {
 								<img src={heroImg} alt="hero-Img" className="w-100" />
 							</div>
 						</Col>
+					</Row>
+				</Container>
+			</section>
+			<section className="pt-0">
+				<Category />
+			</section>
+
+			<section>
+				<Container>
+					<Row>
+						<Col lg="12" className="text-center ">
+							<h5 className="feature__subtitle mb-4">What we serve</h5>
+							<h2 className="feature__title">Just sit back at home</h2>
+							<h2 className="feature__title">
+								We will <span>take care</span>{" "}
+							</h2>
+							<p className="mb-1 mt-4 feature__text">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+								neque.
+							</p>
+							<p className="feature__text">
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil,
+								neque.{" "}
+							</p>
+						</Col>
+						{featureData.map((item, index) => (
+							<Col lg="4" md="4" key={index} className="mt-5">
+								<div className="feature__item text-center p-5 py-3 ">
+									<img
+										src={item.imgUrl}
+										alt="feature-img"
+										className="w-25 mb-3 "
+									/>
+									<h5 className="  fw-bold mb-3 ">{item.title}</h5>
+									<p>{item.desc}</p>
+								</div>
+							</Col>
+						))}
+					</Row>
+				</Container>
+			</section>
+
+			<section>
+				<Container>
+					<Row>
+						<Col lg="12" className="text-center ">
+							<h2>Popular Foods</h2>
+						</Col>
+						<Col lg="12">
+							<div className="food__category d-flex align-items-center justify-content-center gap-4">
+								<button className="all__btn foodBtnActive " onClick={()=>setCategory('ALL')} >All</button>
+								<button className="d-flex align-items-center gap-2" onClick={()=>setCategory('BURGER')} >
+									<img src={foodCategoryImg01} alt="Burguer" />
+									Burguer
+								</button>
+								<button className="d-flex align-items-center gap-2" onClick={()=>setCategory('PIZZA')} >
+									<img src={foodCategoryImg02} alt="Pizza" />
+									Pizza
+								</button>
+								<button className="d-flex align-items-center gap-2" onClick={()=>setCategory('BREAD')} >
+									<img src={foodCategoryImg03} alt="Bread" />
+									Bread
+								</button>
+							</div>
+						</Col>
+						{allProducts.map((item) => (
+							<Col lg="3" md="4" key={item.id} className="mt-5">
+								<ProductCard item={item}></ProductCard>
+							</Col>
+						))}
 					</Row>
 				</Container>
 			</section>
